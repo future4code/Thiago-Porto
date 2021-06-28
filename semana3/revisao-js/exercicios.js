b26c2ad9fe138b17a6f1197eeaa455162c8314de
Thiago Santiago <thiagosantiagoss20@gmail.com>
2021-06-28 11:44:19 -0300

    Exercicio 06

--- semana3/revisao-js/exercicios.js
+++ semana3/revisao-js/exercicios.js
@@ -33,7 +33,20 @@ function retornaQuantidadeElementos(array) {
 
 // EXERCÍCIO 06
 function retornaExpressoesBooleanas() {
+  const booleano1 = true;
+  const booleano2 = false;
+  const booleano3 = !booleano2 ;
+  const booleano4 = !booleano3;
 
+  const bool1 = booleano1 && booleano2 && !booleano4;
+  const bool2 = (booleano1 && booleano2) || !booleano3;
+  const bool3 = (booleano2 || booleano3) && (booleano4 || booleano1);
+  const bool4 = !(booleano2 && booleano3) || !(booleano1 && booleano3);
+  const bool5 = !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3);
+
+  const result = [bool1, bool2, bool3, bool4, bool5];
+
+  return result;
 }
 
 // EXERCÍCIO 07
